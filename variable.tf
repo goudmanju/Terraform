2d278ba61d92e0ce2149e356e068393065440c1a
goudmanju <83080460+goudmanju@users.noreply.github.com>
2025-09-09 16:01:54 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "filename" {
+    default = "pets.txt"
+    type = string
+}
+variable "content" {
+    default = "my dog name is carss"
+}
+
+variable "prefix" {
+    default = "MR"
+    type = string
+}
+
+variable "length" {
+    default = "2"
+    type = number
+}
