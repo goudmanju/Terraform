d9cd8a49ab70ae210a349c84df292854f0bef41f
goudmanju <83080460+goudmanju@users.noreply.github.com>
2025-09-09 16:01:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
-resource "local_file" "my_pet" {
-filename = "pets.txt"
-content = "I Like PETS"
+#resource "local_file" "my_pet" {
+#filename = "pets.txt"
+#content = "I Like PETS"
+
+#}
+resource "random_pet" "candy" {
+    prefix = var.prefix
+    separator = "."
+    length = var.length
 }
+resource "local_file" "my_pet" {
+    filename = var.filename
+    content = var.content
+    }
